84907b30e53384831d9030c8361330dd071aacf3
Prashant P <craftyprash@gmail.com>
2025-06-21 19:47:23 +0530

    PLL 2 col on larger screens not 3 and 3 col for OLL and F2L

--- src/pages/CaseLibrary.tsx
+++ src/pages/CaseLibrary.tsx
@@ -758,7 +758,7 @@ const CaseLibrary: React.FC = () => {
         </div>
 
         {/* Case grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className={ selectedStage === 'PLL' ? 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6' : 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'}>
           {filteredCases.map((cubeCase) => {
             const activeAlgorithm =
               activeTimerId === cubeCase.id && activeAlgorithmId
